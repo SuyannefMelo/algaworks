cc7959a5725395e6f8b25ebb6a16d85547ed9c93
suyanne_melo <suyannefmelo@gmail.com>
2021-08-19 12:47:04 -0300

    Meu primeiro commit

--- 02-02-hello-world-em-javascript/script.js
+++ 02-02-hello-world-em-javascript/script.js
@@ -1,4 +1,5 @@
-var name = 'World' // 'world' é valor da variável e variável pode variar o valor, ou seja, vc pode atribuair qualquer valor pra ela.
+var name = 'World';
+ // 'world' é valor da variável e variável pode variar o valor, ou seja, vc pode atribuair qualquer valor pra ela.
 // name tbm pode mudar pra qualquer nome ate por exemplo "batata". 
-name = 'batata'
-console.log('Hello, ' + name)
+name = 'batata';
+console.log('Hello, ' + name);
