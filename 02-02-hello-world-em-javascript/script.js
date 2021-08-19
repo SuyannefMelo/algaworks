2f22321e549afe2a76595278b43a7ee8c9217295
suyanne_melo <suyannefmelo@gmail.com>
2021-08-19 12:09:03 -0300

    hello word em javascript

---
+++ 02-02-hello-world-em-javascript/script.js
@@ -0,0 +1,4 @@
+var name = 'World' // 'world' é valor da variável e variável pode variar o valor, ou seja, vc pode atribuair qualquer valor pra ela.
+// name tbm pode mudar pra qualquer nome ate por exemplo "batata". 
+name = 'batata'
+console.log('Hello, ' + name)
